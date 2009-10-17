d720381db3a88a83ef10d4db94f18d7c1d7d9e3b
unknown <matthew@.(none)>
2009-10-17 16:04:58 +1300

    Support procname.get on windows
    there is no (simple) way to set the process name on windows so it will
    remain unsupported.

--- procname.c
+++ procname.c
@@ -9,46 +9,77 @@
 
 #include <string.h>
 
-# if defined __linux__
+# if defined(__linux__)
 #include <sys/prctl.h>
 # endif
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#include <Windows.h>
+#include <tchar.h>
+#include <psapi.h>
+#endif
 
 #define PNAME		"procname"
 #define PVERSION	PNAME " library for " LUA_VERSION " / October 2009"
 
+
+#define LUA_BUILD_AS_DLL
+#define LUA_LIB
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
+#if defined(__cplusplus)
+}
+#endif
 
 
 int Pgetname( lua_State* L )
 {
-	int err = 0;
-
-# if defined __linux__
+# if defined(__linux__)
 	char name[17] = {0};
-	err = prctl( PR_GET_NAME, name );
+	int err = prctl( PR_GET_NAME, name );
 	lua_pushstring( L, name );
+	return 1;
+# elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+	TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
+
+	DWORD pid = GetCurrentProcessId();
+	HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid );
+
+	if( hProcess != NULL )
+	{
+		HMODULE hMod;
+		DWORD cbNeeded;
+
+		if ( EnumProcessModules( hProcess, &hMod, sizeof(hMod), &cbNeeded) )
+		{
+			GetModuleBaseName( hProcess, hMod, szProcessName, sizeof(szProcessName)/sizeof(TCHAR) );
+		}
+	}
+
+	CloseHandle( hProcess );
+
+	lua_pushstring( L, szProcessName );
+	return 1;
 # else
 	return luaL_error( L, "procname is not supported on this platform" );
 # endif
-
-	return 1;
 }
 
 
 int Psetname( lua_State* L )
 {
-	int err = 0;
+# if defined(__linux__)
 	const char* name = luaL_checkstring( L, 1 );
-
-# if defined __linux__
-	err = prctl( PR_SET_NAME, name );
-# else
-	return luaL_error( L, "procname is not supported on this platform" );
-# endif
-
+	int err = prctl( PR_SET_NAME, name );
 	return 0;
+# else
+	return luaL_error( L, "procname.set is not supported on this platform" );
+# endif
 }
 
 
